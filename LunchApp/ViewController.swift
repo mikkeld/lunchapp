14246d3e859225ae43634622dd68680bf1c495bf
Mikkel Dengsoee <mikkeld@google.com>
2015-09-20 09:33:07 +0100

    Initial Commit

---
+++ LunchApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LunchApp
+//
+//  Created by Mikkel Dengsøe on 9/20/15.
+//  Copyright © 2015 Mikkel Dengsøe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
